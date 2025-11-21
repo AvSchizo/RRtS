175d4e4b37dc4d2ed29f749e8d372bfc6fee8d83
Jane Doe <paul.d.kolz@gmail.com>
2025-11-21 12:12:10 -0600

    Update Russian_Roulette.py

--- Russian_Roulette.py
+++ Russian_Roulette.py
@@ -1,89 +1,102 @@
-# try camel case for variables and functions
+# try camel case
 
 import random
 import time
 import Flip_Coin
 
+class gunClass:
+    revolution = 0
+    cyl = [0,0,0,0,0]
 
-revolution = 0
-cyl = [0,0,0,0,0]
+gun = gunClass()
 
 
 ## load
 
-bulletChamber = random.randint(1,5) % 5
-cyl[bulletChamber] = 1
+gun.bulletChamber = random.randint(1,5) % 5
+gun.cyl[gun.bulletChamber] = 1
+
+gun.activeChamber = gun.cyl[0]
 
 
-activeChamber = cyl[0]
+def revolve(gun):
+    gun.cyl = gun.cyl[1:] + gun.cyl[:1]
+    if gun.bulletChamber == 0:
+       gun.bulletChamber = (len(gun.cyl)) - 1
+    else:
+       gun.bulletChamber -= 1
+    gun.activeChamber = gun.cyl[0]
+    gun.revolution += 1
 
 
-def revolve():
-   global cyl, bulletChamber, activeChamber, revolution
-   cyl = cyl[1:] + cyl[:1]
-   if bulletChamber == 0:
-       bulletChamber = (len(cyl)) - 1
-   else:
-       bulletChamber -= 1
-   activeChamber = cyl[0]
-   revolution += 1
-
-
-def fire():
-    global activeChamber
-    if activeChamber == 1:
+def fire(gun):
+    if gun.activeChamber == 1:
         print("BANG")
     else:
         print("click")
 
+def gunInfo(gun):
+    print(f"CYLINDER {gun.cyl}")
+    print(f"EASY BC : {(gun.bulletChamber + 1)}")
+    print(f"RAW BULLETCHAMBER : {gun.bulletChamber}")
+    print()
+
+def revPrint(gun):
+    print()
+    print(f"REVOLUTION {gun.revolution}")
+
 
 ##############################
 
-
+## Flip_Coin.py coin flip
 coinFlipV = Flip_Coin.flipCoin()
 
 ### Maybe have a mechanic where the play chooses how hard to spin the cylinder ###
 
 ## round 1 Continue
 r1C = 0
+
 while r1C != 1:
-    revolve()
-    print()
-    print(f"REVOLUTION {(revolution)}")
+    
+    revolve(gun)
+    revPrint(gun)
     time.sleep(1)
 
 
-    print()
-
-    # if coin flip was 1
     if coinFlipV == 1:
+        
         print()
         print("He shoots")
         time.sleep(0.5)
-        if activeChamber == 1:
-            fire()
+        if gun.activeChamber == 1:
+            
+            fire(gun)
+            
             r1c = 1
             Continue = 1
+            
             print("It was a live round, he died")
+            # if have trouble later not continue to next round look here
             exit()
 
 
         else:
-            fire()
+            
+            fire(gun)
             print("It was an empty chamber")
             time.sleep(0.5)
-            revolve()
-            print()
-            print(f"REVOLUTION {(revolution)}")
+            
+            revolve(gun)
+            revPrint(gun)
             time.sleep(1)
     
-    # skipped E turn always if coinFlipV not set to one
+    # skipped E turn always if coinFlipV not set to 1
     else:
         coinFlipV = 1
 
     print()
 
-    ################################NO ITEMS ################################
+    ################################ NO ITEMS ################################
     
     # I don't like 
     Continue = 0
@@ -93,33 +106,52 @@ while r1C != 1:
 
 
         if Target == "me":
-            if activeChamber == 1:
-                fire()
+            
+            if gun.activeChamber == 1:
+                fire(gun)
+                
+                r1c = 1
+                Continue = 1
+                
                 exit()
+                
             else:
+                
                 time.sleep(0.5)
-                fire()
+                
+                fire(gun)
                 print("It was a blank round")
                 time.sleep(0.5)
+                
                 Continue = 1
 
 
         elif Target == "him":
-            if activeChamber == 1:
-                fire()
+            
+            if gun.activeChamber == 1:
+                
+                fire(gun)
                 print("You did it, he's done for")
+                
                 r1c = 1
                 Continue = 1
+                # if have trouble later not continue to next round look here
+                exit()
+                
             else:
-                fire()
+                
+                fire(gun)
                 print()
                 print("Ooh, that's now how the game's played, they turn the gun to you and pull the trigger continuously")
                 time.sleep(1)
+                
                 while True:
-                    fire()
-                    if activeChamber == 1:
+                    
+                    fire(gun)
+                    if gun.activeChamber == 1:
                         exit()
-                    revolve()
+                    
+                    revolve(gun)
 
 
 print("You win")
