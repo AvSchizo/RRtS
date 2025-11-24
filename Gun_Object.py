64170590522998d36b4d1c436108e07ff8ea4f22
Jane Doe <paul.d.kolz@gmail.com>
2025-11-24 09:11:29 -0600

    Update Gun_Object.py

--- Gun_Object.py
+++ Gun_Object.py
@@ -9,3 +9,12 @@ gun = gunClass()
 gun.bulletChamber = random.randint(1,5) % 5
 gun.cyl[gun.bulletChamber] = 1
 gun.activeChamber = gun.cyl[0]
+
+def revolve(gun):
+    gun.cyl = gun.cyl[1:] + gun.cyl[:1]
+    if gun.bulletChamber == 0:
+       gun.bulletChamber = (len(gun.cyl)) - 1
+    else:
+       gun.bulletChamber -= 1
+    gun.activeChamber = gun.cyl[0]
+    gun.revolution += 1
