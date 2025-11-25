708d4ebb2964f2341860a09f9b5562d0eaa572b3
Jane Doe <paul.d.kolz@gmail.com>
2025-11-25 08:18:00 -0600

    Update Item_Test.py

--- Item_Test.py
+++ Item_Test.py
@@ -3,15 +3,15 @@ class itemClass():
 
 item1 = itemClass()
 item1.name = "fish"
-item1.value = 1
+item1.value = 0
 
 item2 = itemClass()
 item2.name = "chocolate"
-item2.value = 1
+item2.value = 0
 
 item3 = itemClass()
 item3.name = "trash"
-item3.value = 1
+item3.value = 0
 
 class characterClass:
     pass
@@ -23,15 +23,12 @@ player.item2 = item2
 player.item3 = item3
 
 
+player.item1.value = 1
 
-
-player.item1.value = 0
-
-
-itemList = [player.item1, player.item2, player.item3]
-
-
-for i in range(len(itemList)):
-    if itemList[i].value > 0:
-        print(itemList[i].name)
-        print(itemList[i].value)
+def itemListPrint(player):
+    itemList = [player.item1, player.item2, player.item3]
+    for i in range(len(itemList)):
+        if itemList[i].value > 0:
+            print(itemList[i].name)
+            print(itemList[i].value)
+itemListPrint(player)
