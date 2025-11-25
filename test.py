e51a68c2e5c5ed264a639bc02c05a747ec0b8c92
Paul Kolz <paul.d.kolz@gmail.com>
2025-11-25 07:05:01 -0600

    works now

--- test.py
+++ test.py
@@ -1,4 +1,27 @@
-import Gun_Object
+class characterClass:
+    pass
 
-gun = Gun_Object.gunClass()
-print(gun.cyl)
+player = characterClass()
+enemy = characterClass()
+
+class itemClass:
+    player.value = 0
+    enemy.value = 1
+
+item1 = itemClass()
+item1.name = "fish"
+
+item2 = itemClass()
+item2.name = "chocolate"
+
+item3 = itemClass()
+item3.name = "trash"
+
+
+itemList = [item1, item2, item3]
+
+print(item1.player.value)
+
+for i in range(len(itemList)):
+    if itemList[i].enemy.value > 0:
+        print(itemList[i].name)
