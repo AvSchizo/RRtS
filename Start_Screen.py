62b816ea8a77b61bb2fb32eb0a2c793f39d71580
Jane Doe <paul.d.kolz@gmail.com>
2025-11-25 20:35:36 -0600

    Update Start_Screen.py

--- Start_Screen.py
+++ Start_Screen.py
@@ -1,1 +1,20 @@
-
+def startScreen():
+    while True:
+        userStartOption = input("Russian Roulette The Sequal\n|start\n|exit\n|cheats\nanswer: ")
+        if userStartOption == "exit":
+            exit()
+        elif userStartOption == "start":
+            print()
+            print("Oh, that's-a baseball")
+            print()
+            break
+        elif userStartOption == "cheats":
+            print()
+            print("Sorry, feature not in game yet, stay tuned")
+            print()
+        else:
+            print()
+            print("Sorry, I don't understand")
+            print()
+    
+    print("Game has started : test")
